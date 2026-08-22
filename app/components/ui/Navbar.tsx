b717096eef8f24fecbd4f59fc07a43fc5978616e
Rxunak <raunakuk2016@gmail.com>
2026-08-22 15:15:37 +0100

    RXB-navbarMenu enhance Navbar with search input and button components; add InputGroup and Textarea components

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -1,22 +1,44 @@
 import { Input } from "../ui/input";
 import React from "react";
 import { Bookmark } from "lucide-react";
+import { SearchIcon } from "lucide-react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+  InputGroupTextarea,
+} from "../ui/input-group";
+import { Button } from "./button";
 
 function Navbar() {
   return (
-    <main className="flex justify-between pl-10 pr-10 pt-5 pb-5 border bg-bg h-auto">
+    <main className="flex justify-between pl-10 pr-10 pt-5 pb-5 border h-auto bg-white">
       <div className="flex items-center">
         <h1 className="text-h4 text-text">Where to Watch</h1>
       </div>
 
       <div className="flex gap-6">
-        <Input className="w-50" />
-        <div className="flex gap-1.5 items-center">
-          <Bookmark />
-          <p>Watchlist</p>
-          <span>2</span>
+        <InputGroup className="w-70">
+          <InputGroupInput placeholder="Search a film or series..." />
+          <InputGroupAddon>
+            <SearchIcon />
+          </InputGroupAddon>
+        </InputGroup>
+        <div className="flex gap-1.5 items-center ">
+          <div className="flex gap-2 items-center hover:text-amber-700 cursor-pointer">
+            <Bookmark className="size-4" />
+            <p className="">Watchlist</p>
+          </div>
+
+          <span className="bg-amber-100 w-5 rounded-sm flex justify-center">
+            2
+          </span>
         </div>
-        <button>Dark</button>
+        <Button variant="outline" className="cursor-pointer">
+          Dark
+        </Button>
       </div>
     </main>
   );
