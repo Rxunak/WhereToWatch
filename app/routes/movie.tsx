5755b3e1377f57324b76d2078245505aed002594
Rxunak <raunakuk2016@gmail.com>
2026-08-29 10:24:46 +0100

    feat(movie): enhance layout and navigation for movie page

--- app/routes/movie.tsx
+++ app/routes/movie.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { MoveLeft } from "lucide-react";
+import { useNavigate } from "react-router";
 
 function movie() {
+  const navigate = useNavigate();
   return (
-    <main>
+    <main className="bg-white min-h-screen p-10">
+      <section
+        className="flex gap-3 text-amber-700 cursor-pointer"
+        onClick={() => navigate("/search")}
+      >
+        <MoveLeft />
+        <h1>Back to results</h1>
+      </section>
       <section></section>
     </main>
   );
